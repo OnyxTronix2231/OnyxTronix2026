15271c92988914c215679107ae1b87e1ce35d606
DoriZigart <dori.zigart@gmail.com>
2026-01-10 23:05:32 +0200

    updated 2026

--- src/main/java/frc/robot/lib/DriverUtils.java
+++ src/main/java/frc/robot/lib/DriverUtils.java
@@ -4,14 +4,14 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Superstructure;
 
 public class DriverUtils {
-    public static InstantCommand setWantedState(Superstructure.WantedSuperState state) {
-        return new InstantCommand(() -> Superstructure.getInstance().setWantedSuperState(state));
-    }
+    // public static InstantCommand setWantedState(Superstructure.WantedSuperState state) {
+    //     return new InstantCommand(() -> Superstructure.getInstance().setWantedSuperState(state));
+    // }
 
-    public static Superstructure.WantedSuperState setWantedStateAsToggle(Superstructure.WantedSuperState state) {
-        if (Superstructure.getInstance().getWantedSuperState() == state)
-            return Superstructure.WantedSuperState.DEFAULT;
-        return state;
-    }
+    // public static Superstructure.WantedSuperState setWantedStateAsToggle(Superstructure.WantedSuperState state) {
+    //     if (Superstructure.getInstance().getWantedSuperState() == state)
+    //         return Superstructure.WantedSuperState.DEFAULT;
+    //     return state;
+    // }
 
 }
