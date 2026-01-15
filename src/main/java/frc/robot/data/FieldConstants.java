8d2033a88b8add5ac8aa268404b7b9ead1521215
DoriZigart <dori.zigart@gmail.com>
2026-01-15 23:34:27 +0200

    added

--- src/main/java/frc/robot/data/FieldConstants.java
+++ src/main/java/frc/robot/data/FieldConstants.java
@@ -1,5 +1,9 @@
 package frc.robot.data;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class FieldConstants {
-    public s
+    public static Pose2d BLUE_RIGHT_BUMP  = new Pose2d(4.625594,2.51079,new Rotation2d(Math.toRadians(0)));
+
 }
