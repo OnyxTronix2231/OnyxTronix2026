6aacbd9d6f42cb608dece7c6737ec7899b5b00a2
guygok <guykejzman@gmail.com>
2026-01-17 13:15:32 +0200

    Update ObjectPose2dCalculator.java

--- src/main/java/frc/robot/objectDetection/ObjectPose2dCalculator.java
+++ src/main/java/frc/robot/objectDetection/ObjectPose2dCalculator.java
@@ -1,5 +1,28 @@
 package frc.robot.objectDetection;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+
 public class ObjectPose2dCalculator {
+
+    String limelightName;
+
+    public ObjectPose2dCalculator(String limelightName) {
+        this.limelightName = limelightName;
+    }
+
+    public double getTX() {
+        return NetworkTableInstance.getDefault().getTable(limelightName).getEntry("llpython").getDoubleArray(new double[4])[0];
+    }
+
+    public double getTY() {
+        return NetworkTableInstance.getDefault().getTable(limelightName).getEntry("llpython").getDoubleArray(new double[4])[1];
+    }
+
+    public double getThor() {
+        return NetworkTableInstance.getDefault().getTable(limelightName).getEntry("llpython").getDoubleArray(new double[4])[14];
+    }
+
+
 
 }
